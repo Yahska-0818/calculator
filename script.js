76599d705cc49726925a041ed9848b88353db5bd
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-22 06:43:12 +0000

    Added calculator operations and main operator function

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World")
+function add(num1,num2) {
+    return parseInt(num1)+parseInt(num2)
+}
+
+function subtract(num1,num2) {
+    return parseInt(num1)-parseInt(num2)
+}
+
+function multiply(num1,num2) {
+    return parseInt(num1)*parseInt(num2)
+}
+
+function divide(num1,num2) {
+    return parseInt(num1)/parseInt(num2)
+}
+
+function operate(num1,operator,num2) {
+    if (operator === "+"){
+        return add(num1,num2)
+    }
+    else if (operator === "-") {
+        return subtract(num1,num2)
+    }
+    else if (operator === "*") {
+        return multiply(num1,num2)
+    }
+    else if (operator === "/") {
+        return divide(num1,num2)
+    }
+}
+
+let num1 = prompt("Enter num1")
+let operator = prompt("Enter an operator")
+let num2 = prompt("Enter num 2")
+
+console.log(operate(num1,operator,num2))
